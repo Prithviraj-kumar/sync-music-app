ca67cc0bd8221bc3d9d74eaf34950a3aa9ae4aab
Prithviraj kumar <www.rohitrajking@gmail.com>
2026-04-10 07:08:53 +0530

    Fresh frontend folder with all components

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10B981',
+        dark: '#040914',
+        darker: '#0B1120',
+        card: '#1E293B'
+      }
+    },
+  },
+  plugins: [],
+}
